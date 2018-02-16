3ec6b135de655d0e754cdec1b560df831bb74464
Everett <everett.bowline@gmail.com>
2018-02-16 03:30:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,19 +16,17 @@ function nowServing(line, name) {
 }
 
 function currentLine(line) {
+if (line.length === 0) {
+  return "The line is currently empty."
+}
+  
 var deliLine = []
 
   for (let i = 0, l = line.length; i < l; i++ ) {
     
     deliLine.push(`${i + 1}. ${line[i]}`)
     
-  if (deliLine.length > 0) {
+  }
     return `The line is currently: ${deliLine[i]},`
     
-  }
-  
-    else {
-      return "The line is currently empty."
-    }
-  }  
 }
