b1fe69beaf598104113642ec72ea931d24d3d851
Everett <everett.bowline@gmail.com>
2018-02-16 03:25:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
  var deliLine = []
  
   for (var i = 0, l = deliLine.length; i < l; i++ ) {
-    deliLine.push(`${i + 1}. ${line[i]}`)
+    deliLine.push(`${i + 1}. ${deliLine[i]}`)
     
   if (deliLine.length > 0) {
     return `The line is currently: ${deliLline[i]},`
