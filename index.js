846b9fa6786e9a7de2582abeb62dc11de83b9fa6
Everett <everett.bowline@gmail.com>
2018-02-16 03:00:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var katzDeli = []
 
-function takeANumber(katzDeliLine) {
-  var katzDeliLine = []
+function takeANumber(line, name) {
+  
   for (let i = 0; i < 10; i++) {
-    array.push(`${katzDeliLine[i]}`)
+    array.push(`${line[name]}`)
   }
-  return "Welcome, " + katzDeliLine[i] + ". You are number " + i  + " in line"
+  return "Welcome, " + line[i] + ". You are number " + i  + " in line"
 }
