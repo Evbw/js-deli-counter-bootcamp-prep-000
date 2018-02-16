d6b748ed397d91bae08d5e711485e3d36babe520
Everett <everett.bowline@gmail.com>
2018-02-16 03:01:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeli = []
 function takeANumber(line, name) {
   line.push(name)
   
-  return `Welcome, ${name} + line[i] + ". You are number " + i  + " in line"
+  return `Welcome, ${name}. You are number " + i  + " in line"
 }
